5523f5ad5f1cacb01a2582af4e9fe5b65e6caa02
mshah22315 <65419016+mshah22315@users.noreply.github.com>
2023-09-15 11:12:43 -0500

    Syntactical fixes

--- MainCharacter.java
+++ MainCharacter.java
@@ -1,22 +1,22 @@
 package tbrCode;
 
 public class MainCharacter {
-	private String name;
-	private int hp;
-	private int attack;
-	private int xp;
+    private String name;
+    private int hp;
+    private int attack;
+    private int xp;
     private int level;
 
     public MainCharacter(String name) {
     	this.name = name;
-	    this.hp = 100;
-	    this.attack = 10;
-	    this.xp = 0;
-      this.level = 1;
+	this.hp = 100;
+	this.attack = 10;
+	this.xp = 0;
+      	this.level = 1;
     }
 
 	public String getName() {
-		return name;
+	    return name;
 	}
 
 	public int getHP() {
@@ -28,7 +28,7 @@ public class MainCharacter {
 	}
 	
 	public int getXP() {
-        return xp;
+            return xp;
     }
 
     public int getLevel() {
@@ -44,18 +44,18 @@ public class MainCharacter {
 	}
 	
 	public void gainXP(int xp) {
-        this.xp += xp;
-        if (this.xp >= level * 100) {
-            levelUp();
-        }
-    }
+            this.xp += xp;
+            if (this.xp >= level * 100) {
+                levelUp();
+            }
+    	}
 	
 	private void levelUp() {
-        level++;
-        hp += 20; // Increase health when leveling up
-        attack += 5; // Increase attack when leveling up
-        System.out.println("Level up! " + name + " is now level " + level + "!");
-    }
+            level++;
+            hp += 20; // Increase health when leveling up
+            attack += 5; // Increase attack when leveling up
+	    System.out.println("Level up! " + name + " is now level " + level + "!");
+    	}
 
 	public boolean isAlive() {
 	    return hp > 0;
